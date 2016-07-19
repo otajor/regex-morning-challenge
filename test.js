ea96ae0413085858e1eaf72fe8dd21db315be279
Owen TM <oturnermajor@gmail.com>
2016-07-19 09:32:01 +0100

    only test file

---
+++ test.js
@@ -0,0 +1,48 @@
+/* eslint-env mocha */
+const expect = require('chai').expect
+
+describe('isValidHex should', () => {
+  it('return true if given string is valid hexadecimal (3 or six letters a-f or numbers)', () => {
+    expect(isValidHex('1D1f21')).to.be.ok
+    expect(isValidHex('89D387')).to.be.ok
+    expect(isValidHex('b0f')).to.be.ok
+  })
+  it('return false if given string is not valid hexadecimal', () => {
+    expect(isValidHex('obviouslyNotValid')).not.to.be.ok
+    expect(isValidHex('XYZ')).not.to.be.ok
+  })
+})
+
+describe('isSmaller (using regex.test or string.match) should', () => {
+  it('return true if string is a number smaller than 55', () => {
+    expect(isSmaller('3')).to.be.ok
+    expect(isSmaller('10')).to.be.ok
+    expect(isSmaller('25')).to.be.ok
+  })
+  it('return false if string is a number greater than 55', () => {
+    expect(isSmaller('56')).not.to.be.ok
+    expect(isSmaller('129')).not.to.be.ok
+  })
+})
+
+describe('findProperNouns (using regex.exec or string.match) should', () => {
+  it('return a proper noun in a given string, in an array', () => {
+    const string = 'proper noun in a sentence about Paris.'
+    expect(findProperNouns(string)).to.deep.equal(['Paris'])
+  })
+  it('return several proper nouns in a given string, ignoring those following full stops', () => {
+    const string2 = 'Several words in a string. Like Berlin, Rotterdam, Founders & Coders.'
+    expect(findProperNouns(string2)).to.deep.equal(['Berlin', 'Rotterdam', 'Founders', 'Coders'])
+  })
+})
+
+describe('search function should', () => {
+  const dict = ['abacus', 'polywhirl', 'obama', 'shorts', 'frisbee', 'potato']
+  it('find the elements of an array that begin with a given string', () => {
+    expect(search(dict, 'ab')).to.deep.equal(['abacus'])
+    expect(search(dict, 'po')).to.deep.equal(['polywhirl', 'potato'])
+  })
+  it('return empty array if no matches are present', () => {
+    expect(search(dict, 'no')).to.deep.equal([])
+  })
+})
